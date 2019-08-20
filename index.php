75a60751c4af7e4cbaeec215da083d283c603117
sohelmagento <sohel@marketingmindz.in>
2019-08-20 17:33:54 +0530

    Initial commit

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Git Test</title>
+	<link rel="stylesheet" href="style.css"  />
+</head>
+<body>
+<div class="main">
+	<h1>Hello, i am using github.</h1>
+</div>
+</body>
+</html>
